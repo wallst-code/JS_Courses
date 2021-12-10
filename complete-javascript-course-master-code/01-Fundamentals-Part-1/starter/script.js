faac9512ad8c714fce490d239a2886c842f1497f
wallst-code <senit@senitlutgen.com>
2021-12-10 08:35:15 -0800

    JS lessons

--- complete-javascript-course-master-code/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master-code/01-Fundamentals-Part-1/starter/script.js
@@ -6,5 +6,5 @@ console.log('Jonas');
 let firstName = 'jonas';
 console.log(firstName);
 
-
+//The value holds the variable type not the variable itself. I think the variable holds the value which holds the data type. 
 
