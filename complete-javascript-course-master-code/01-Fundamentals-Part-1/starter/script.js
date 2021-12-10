b1a389e26c6fb3b7f0f9255b17879d14d2022081
wallst-code <senit@senitlutgen.com>
2021-12-10 09:42:21 -0800

    JS Lessons

--- complete-javascript-course-master-code/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master-code/01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,56 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log('Jonas');
+// console.log('Jonas');
 
-let firstName = 'jonas';
-console.log(firstName);
+// let firstName = 'jonas';
+// console.log(firstName);
+// let dogs;
+// dogs = null;
+// dogs = 1/1;
 
-//The value holds the variable type not the variable itself. I think the variable holds the value which holds the data type. 
+// //The value holds the variable type not the variable itself. I think the variable holds the value which holds the data type. 
 
+// let javascriptisfun = true;
+
+
+// console.log(typeof dogs);
+// alert(dogs);
+
+// var - avoid this declaration in modern javascript. This was used prior to ES6. It works much like let. 
+// let - value can be changed
+// const - value cannot be changed - immutable variable. You cannot leave a const undefined it must be initialized. 
+// in JS you do not have to declare with let, but that is a bad practice. 
+
+// let x = 10 + 15;
+// x += 40;
+// x -= 30;
+// x *= 2;
+// x /= 2;
+// x **= 2;
+// x++;
+// x--;
+// let y = 50;
+// let c = 10;
+// console.log(c > y);
+// console.log(y > c);
+// console.log(x);
+
+// console.log((100 - 50) > (90 - 70), x, c, y);
+// let jim = "jimmy";
+// let george = "geo";
+// let properG = "get lost now...";
+
+// let x, y;
+// x =10;
+// y = 10;
+
+// console.log(x, y);
+
+// Coding Challenge #1
+
+let fName = "Johnny";
+let fNameNew = `${fName}`;
+
+
+console.log(fNameNew);
