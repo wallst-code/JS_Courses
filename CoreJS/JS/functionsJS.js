0e9dcad761f3adbc04581967b1305cc7e2a21efd
wallst-code <senit@senitlutgen.com>
2021-12-13 13:58:36 -0800

    Functions in JS

--- CoreJS/JS/functionsJS.js
+++ CoreJS/JS/functionsJS.js
@@ -1,4 +1,19 @@
 
+function add(x = 6, y = 6) { // Here we set default values if nothing is passed. 
+    console.log(`Hello from my add function - you passed ${x} and ${y} - the answer is ${x+y}`);
+    return x + y;
+}
+
+console.log(add(2,3));
+console.log(add(0,0));
+
+let result = add(2, 5);
+console.log(result); // The inner function runs and returns the value back to the calling function (in this case that is console.log()). 
+
+console.log(add(2, 5) + result);
+
+console.log(add()); // this uses the default values that are set.
+console.log(add(0,)); // this would run as well. 
 
 
 
