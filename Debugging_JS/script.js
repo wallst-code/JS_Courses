6fee575b44c904bd707dba150ce891e3649be493
wallst-code <senit@senitlutgen.com>
2021-12-14 13:22:59 -0800

    debugging lesson

---
+++ Debugging_JS/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Debugging JavaScript
+
+(function(app){
+    'use strict';
+    app.divide = function(x, y) {
+        return x / y;
+    };
+    app.complicatedFormula = function(x) {
+        let result = x * 3 + 2;
+        x += 4;
+        result += this.divide(result, 2);
+        return result;
+    };
+})(window.app = window.app || {});
+
+console.log(app.complicatedFormula(23));
+
