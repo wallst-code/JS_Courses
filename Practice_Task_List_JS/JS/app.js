44a82dab1ec0911f01442d4b8d45b6196a07a7c1
wallst-code <senit@senitlutgen.com>
2021-12-17 18:42:34 -0800

    Promises Demo

--- Practice_Task_List_JS/JS/app.js
+++ Practice_Task_List_JS/JS/app.js
@@ -11,6 +11,15 @@
     const pageItems = {};
     // const taskList = document.getElementById('taskList');
 
+    app.promisesStartup = function() {
+        pageItems.loadData = document.getElementById('loadData');
+        pageItems.waitIndicator = document.getElementById('wait-indicator');
+
+        pageItems.loadData.addEventListener('click', loadChainedPromisedData);
+
+
+    }
+
     // Using this app.todoStartup make this the only function available outside the others are internal.  test this in the html by typing in app. and see what is visible. 
     app.todoStartup = function(){
         const frm = document.getElementById('taskForm');
@@ -25,6 +34,43 @@
 
         loadFromStorage();
     };
+
+    function loadSimplePromiseData(e){
+        pageItems.waitIndicator.style.display = 'block';
+        const promise = new Promise(function(resolve, reject){
+            setTimeout(() => reject("Rejected the promise"), 3000);
+        });
+
+        // Here we have a full "then clause" with success code and failure code. 
+        // Basic promise setup
+        promise
+        .then(
+            result => console.log(result),
+            reason => console.error(reason)
+        )
+        .finally(() => {
+            console.log('This is now complete');
+            pageItems.waitIndicator.style.display = 'none';
+        })
+        //Chaining together with promises...the finally is optional and used if we need cleanup. 
+    }
+
+    function loadChainedPromisedData(e){
+        const promise = new Promise(function(resolve, reject) {
+            setTimeout(() => resolve('Promise #1'), 3000);
+        });
+
+        promise.then(result => {
+            console.log('Promise #1 Succeeded');
+            return new Promise(function(resolve, reject) {
+                setTimeout(() => resolve('Promise #2'), 2000);
+            });
+        })
+        .then(result => {
+            console.log('Promise #2 Succeeded');
+        })
+        .finally(() => console.log('We have now completed all promises'));        
+    }
 
     function loadFromStorage(){
         const itemsString = localStorage.getItem('taskList');
@@ -56,14 +102,11 @@
 
     function addTask(e) {
         e.preventDefault;
-
         const li = document.createElement('li'); //create the element
         li.innerText = pageItems.taskInput.value.toUpperCase(); //set the text
         pageItems.taskList.appendChild(li); //append the text
         pageItems.taskInput.value = ''; //clears input box
-        saveToStorage();
-        
-       
+        saveToStorage();   
     }
 
     function completeTask(e) {
@@ -73,8 +116,7 @@
         } else {
             e.target.classList.add('completed-task'); //from css class - adds strike through to completed tasks upon click. 
         }
-        saveToStorage()
-        
+        saveToStorage()        
     }
 
     function removeCompletedTasks(e) {
@@ -89,7 +131,6 @@
             }
         });
         saveToStorage()
-
     }
     
 })(window.app = window.app || {});
