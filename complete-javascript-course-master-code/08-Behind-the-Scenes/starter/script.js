961614b922bbc7be3cf0aa022cb07ee14a56cb63
wallst-code <senit@senitlutgen.com>
2022-01-07 18:57:21 -0800

    clean up

--- complete-javascript-course-master-code/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master-code/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+
+  function printAge() {
+    const output = `${firstName} is ${age}, born in ${birthYear}.`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1986) {
+      const str = `Oh, and you are a millenial, ${firstName}.`;
+      console.log(str);
+      //   var millenial = true;
+
+      function add(a, b) {
+        return a + b;
+      }
+      console.log(add(2, 2));
+    } else {
+      const str = `${firstName}, you're not a millenial.`;
+      console.log(str);
+    }
+    // console.log(millenial);
+    // console.log(add(2, 3));
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Senit';
+calcAge(1985);
