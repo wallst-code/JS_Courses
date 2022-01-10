77205ca5d795d9890e79b2cc14dd9b7645d6e430
wallst-code <senit@senitlutgen.com>
2022-01-09 17:52:38 -0800

    lesson changes

--- complete-javascript-course-master-code/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master-code/08-Behind-the-Scenes/starter/script.js
@@ -1,33 +1,128 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2022 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2022 - birthYear;
 
-  function printAge() {
-    const output = `${firstName} is ${age}, born in ${birthYear}.`;
-    console.log(output);
+//   function printAge() {
+//     const output = `${firstName} is ${age}, born in ${birthYear}.`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1986) {
-      const str = `Oh, and you are a millenial, ${firstName}.`;
-      console.log(str);
-      //   var millenial = true;
+//     if (birthYear >= 1981 && birthYear <= 1986) {
+//       const str = `Oh, and you are a millenial, ${firstName}.`;
+//       console.log(str);
+//       //   var millenial = true;
 
-      function add(a, b) {
-        return a + b;
-      }
-      console.log(add(2, 2));
-    } else {
-      const str = `${firstName}, you're not a millenial.`;
-      console.log(str);
-    }
-    // console.log(millenial);
-    // console.log(add(2, 3));
-  }
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       console.log(add(2, 2));
+//     } else {
+//       const str = `${firstName}, you're not a millenial.`;
+//       console.log(str);
+//     }
+//     // console.log(millenial);
+//     // console.log(add(2, 3));
+//   }
 
-  printAge();
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
-const firstName = 'Senit';
-calcAge(1985);
+// const firstName = 'Senit';
+// calcAge(1985);
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAgeArrow(1980);
+
+// const jonas = {
+//   firstName: 'Jonas',
+//   year: 1991,
+//   calcAge: function (year) {
+//     console.log(2037 - this.year);
+//     console.log(this);
+
+//     //Solution 1 - old way
+//     // const self = this; //can be called self or that
+//     // const isMillenial = function () {
+//     //   console.log(self.year >= 1981 && self.year <= 1996);
+//     //   console.log(self);
+//     // };
+
+//     // Solution 2 -- new solution for ES6
+//     const isMillenial = () => {
+//       console.log(this.year >= 1981 && this.year <= 1996);
+//       console.log(this);
+//     };
+//     isMillenial();
+//   },
+
+//   greet: () =>
+//     console.log(
+//       `Hey ***${this.firstName}*** arrow functions go to the global obj and this is not defined there. ***this = ${this}***so it goes to the global window obj.`
+//     ),
+
+//   greet2: function () {
+//     console.log(`How does this resolve ${this}?`);
+//     console.log(`Hey ${this.firstName}`);
+//   },
+// };
+
+// jonas.greet();
+// jonas.greet2();
+
+// jonas.calcAge();
+
+// const addExpr = function (a, b) {
+//   console.log(arguments);
+//   return a + b;
+// };
+
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age);
+// console.log(oldAge);
+
+// const me = {
+//   name: 'Jonas',
+//   age: 30,
+// };
+
+// const friend = me;
+// friend.age = 27;
+// console.log('Friend:', friend);
+// console.log('Me:', me);
+
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+
+console.log(lastName);
+console.log(oldLastName);
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+// Here we can create a new separate copy using the Object.assign().
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
--- complete-javascript-course-master-code/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master-code/09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,170 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order Received: Your ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to: ${address} by ${time}.`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}.`
+    );
+  },
 };
+
+//Get user input and place the data into an array that then you can use the spread operator on.
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3?'),
+// ];
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+//Since the 2018 ES6 update - the spread operator can be used on objects even though they are not iterables.
+//Objects - create a brand new object by copying the properties and you can add more using the spread operator.
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log('new restaurant obj + additonal properties ', newRestaurant);
+console.log('original restaurant obj ', restaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'New name';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+// const arr = [7, 8, 9];
+// const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArray);
+
+// //Using the spread operator
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+
+// const blue = [2, 5, 60];
+// const newNewArr = [10, ...blue];
+// console.log(newNewArr);
+
+// console.log(...newNewArr);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// //merge arrays and make a shallow copy of an array
+// const mainMenuCopy = [...restaurant.mainMenu]; // a shallow copy of the array.
+
+// //join two or more arrays
+// const joinMenus = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(joinMenus);
+
+// //The spread operator works on all iterables in JS --- arrays, strings, maps, sets, but not objects.
+
+// const str = 'Jonas';
+// const letters = [...str, '', 'S.'];
+// console.log(letters);
+
+// console.log(...str);
+
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Via del Sole, 21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
+
+// restaurant.orderDelivery({
+//   address: 'Via del Sole, 21',
+//   starterIndex: 1,
+// });
+
+// //object destructing...unlike deconstructing an array, with an object the order does not matter.
+// // this is very useful for getting info from API's - deconstrucing the JSON
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// // If we want to rename the properties from the object to our own variable names.
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// // Default values if it does not exist within the object.
+// const { menu = [], starterMenu = [] } = restaurant;
+// console.log(menu, starterMenu);
+
+// // Mutating variables:
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+
+// // Must wrap this in () to work - or JS expects a code block when it sees {}.
+// ({ a, b } = obj);
+
+// console.log('a =', a, 'b =', b);
+
+// // Nested Object Deconstructing - example: restaurant obj, opening hours
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log('open = ', open, 'close = ', close);
+
+// // const arr = [2, 3, 4];
+// // const a = arr[0];
+// // const b = arr[1];
+// // const c = arr[2];
+
+// // // This is the destructing assignment.
+// // const [x, y, z] = arr;
+// // console.log(x, y, z);
+// // // so the structure above breaks the array into separate variables.
+// // // the original array is still intact.
+
+// // let [main, , secondary] = restaurant.categories;
+// // console.log('main = ', main, '\nsecondary = ', secondary);
+
+// // const temp = main;
+// // console.log('main: ', main);
+// // main = secondary;
+// // console.log('main: ', main);
+// // secondary = temp;
+// // console.log('secondary: ', secondary);
+// // console.log('main =', main, '\nsecondary = ', secondary);
+
+// // // Just reassigning the two variables and we do not need the temp variable in the middle.
+// // [main, secondary] = [secondary, main];
+// // console.log('main =', main, '\nsecondary = ', secondary);
+
+// // console.log(restaurant.order(2, 0));
+
+// // // Recieve two return values from a function.
+// // const [starterCourse, mainCourse] = restaurant.order(2, 0);
+// // console.log('starterCourse =', starterCourse, '\nmainCourse =', mainCourse);
+
+// // // What about decontrusting nested arrays?
+// // const nested = [2, 4, [5, 6]];
+// // // const [i, , j] = nested;
+// // // console.log(i, j);
+// // const [i, , [j, k]] = nested;
+// // console.log(i, j, k);
+
+// // // Setting default variables for when we destruct them...for unknown array lengths.
+// // // const [p, q, r] = [8, 9];
+// // // console.log(p, q, r);
+// // // since our decontruction was longer than the actual array that was targeted, it left the unused variable(s) as undefined.
+// // // but we can set default values also.
+// // const [p = 1, q = 1, r = 1] = [8, 9];
+// // console.log(p, q, r);
