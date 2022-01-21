85cae4c70b75c3d5b4aef2015e31518fd27b5534
wallst-code <senit@senitlutgen.com>
2022-01-21 09:43:58 -0800

    lessons

--- complete-javascript-course-master-code/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master-code/11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,83 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+//Arrays are objects and have builtin methods.
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//Array Methods
+//Slice does not mutate - it just creates a new array wit hthe extracted parts.
+// Note the end number is not included, but the starting number is.
+// Calculate the last index number that will be included by subtracting the start from the end = index.
+// Example: 1 - 4 = 3, and so we would get up to index 3. In this case that would be 'd'.
+// Copying from the end of an array by starting with a negative number -- with (-2) you would get the last two elements in the array.
+// -1 is the last element of any array. Using (1, -2) would start with the [1] index position and the -2 would not be included - the end points are not included.
+console.log(arr.slice(2));
+// c, d, e
+console.log(arr.slice(1, 4));
+// b, c, d
+console.log(arr.slice(-2));
+// d, e
+console.log(arr.slice(1, -2));
+// b, c
+console.log(arr.slice());
+// no parameters creates a shallow copy of the entire array.
+console.log([...arr]);
+// This creates a new array and uses the spread operator to expand the original array - same as the sopy.
+// You can use slice() or ...the spread operator, but the slice() will allow you to chain methods.
+
+// The SPLICE Method - this one does mutate the original array.
+
+console.log(arr.splice(2));
+// c, d, e
+console.log(arr);
+// But the original array has been mutated to a, b. The extracted elements are removed.
+// Common Use Case - remove the last element from an array.
+console.log(arr.splice(-1));
+// b --- because we already mutated the array above.
+
+let arr2 = ['a', 'b', 'c', 'd', 'e'];
+// Use Splice to remove two inner elements: Here the ending number is included.
+console.log(arr2.splice(1, 2));
+// b, c
+console.log(arr2);
+// [a, b, e]
+
+//REVERSE - reverses the order and mutates the original array
+let arr3 = ['e', 'd', 'c', 'b', 'a'];
+console.log(arr3, 'original array');
+console.log(arr3.reverse(), 'reversed array');
+console.log(arr3, 'Reverse mutates the original array');
+
+// CONCAT - concatenates arrays - makes a new array and Does not Mutate the Original
+//Choice of methods - does not matter except for chaining methods.
+const letters = arr2.concat(arr3);
+console.log(letters, 'Concatenated arrays 2 & 3');
+// Alternative method - use multiple spread operators:
+console.log([...arr, ...arr2, ...arr3], 'Using spread operators');
+
+// JOIN Method - does not mutate the original array
+console.log(letters.join(' - '), 'Using Join');
+// a - b - c - d - e ....
+console.log(letters, 'join does not mutate');
+////////////////////////////////////////////////////////////////
+//ES 2022 At Method - returns a value of an array at a specified index position.
+//replaces [] with a method ().
+// Why care? .at() is better for using the end of an array and if you want to method chain the .at() is better.
+//The .at() also works on strings.
+const array1 = [23, 11, 12, 15];
+console.log(array1, 'original array');
+console.log(array1.at(0), 'using the .at(0) to find the value of an index');
+console.log('give the same result as array1[0]', array1[0]);
+
+console.log(
+  array1[array1.length - 1],
+  ' if we wanted to get the last value the old way array1[array1.length - 1]'
+);
+console.log(array1.at(-1), 'with the .at(-1)'); //you could also do this with slice.
+
+//Using .at() on a string:
+console.log('Senit'.at(-1), '.at() can be used on strings');
+
+//////////////////////////////////////////////////////////////
+//Looping Arrays
