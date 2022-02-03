5d21c06ceb1c506a1785874b4df095b4d733dda1
wallst-code <senit@senitlutgen.com>
2022-02-03 14:25:12 -0800

    DOM Lessons

--- complete-javascript-course-master-code/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master-code/13-Advanced-DOM-Bankist/starter/script.js
@@ -32,3 +32,53 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////// Selecting, Creating, Deleting Elements //////////////
+/////SELECTING ELEMENTS
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+console.log(document.querySelector('.header'));
+
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+
+const buttons = document.getElementsByTagName('button');
+console.log(buttons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and Inserting Elements
+// .insertAdjacentHTML
+
+//creates a DOM element that is stored in message - not used yet.
+const message = document.createElement('div');
+message.classList.add('cookie-message'); // add a class to the element
+
+//To put on the page, we must insert it.
+// message.textContent =
+//   'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+const header = document.querySelector('.header');
+// header.prepend(message);
+//Prepend makes it the first child of the DOM element
+// header.append(message); // Makes it the last child
+// header.append(message.cloneNode(true));
+
+header.before(message);
+// header.after(message);
+
+//Delete Elements - deletes once the button is clicked
+document
+  .querySelector('.btn--close-cookie') //we could use the classList as well.
+  .addEventListener('click', function () {
+    message.remove();
+  });
+
+///////// Styles, Attributes, and Classes
