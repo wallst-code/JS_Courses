6b2f7b965930f7980040ce750397900724f74a72
wallst-code <senit@senitlutgen.com>
2022-02-04 17:32:00 -0800

    Dom Lessons

--- complete-javascript-course-master-code/13-Advanced-DOM-Bankist/starter/script.js
+++ complete-javascript-course-master-code/13-Advanced-DOM-Bankist/starter/script.js
@@ -33,90 +33,138 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//Implement the smooth scroll of the Learn More link.
+const btnScroll = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScroll.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log(e);
+  console.log(e.target);
+  console.log(
+    'Current scroll (x,y) x is from the left and y is from the top',
+    window.pageXOffset,
+    pageYOffset
+  );
+  console.log(
+    'height and width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+  // old way
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // ); //x and y positions
+  // window.scrollTo(0, 700);
+
+  // Alt way with an object
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  // new and improved way - much shorter
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
 ///////////// Selecting, Creating, Deleting Elements //////////////
-/////SELECTING ELEMENTS
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// /////SELECTING ELEMENTS
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
-document.querySelector('.header');
-console.log(document.querySelector('.header'));
+// document.querySelector('.header');
+// console.log(document.querySelector('.header'));
 
-const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
 
-document.getElementById('section--1');
+// document.getElementById('section--1');
 
-const buttons = document.getElementsByTagName('button');
-console.log(buttons);
+// const buttons = document.getElementsByTagName('button');
+// console.log(buttons);
 
-console.log(document.getElementsByClassName('btn'));
+// console.log(document.getElementsByClassName('btn'));
 
-// Creating and Inserting Elements
-// .insertAdjacentHTML
+// // Creating and Inserting Elements
+// // .insertAdjacentHTML
 
-//creates a DOM element that is stored in message - not used yet.
-const message = document.createElement('div');
-message.classList.add('cookie-message'); // add a class to the element
+// //creates a DOM element that is stored in message - not used yet.
+// const message = document.createElement('div');
+// message.classList.add('cookie-message'); // add a class to the element
 
-//To put on the page, we must insert it.
-// message.textContent =
-//   'We use cookies for improved functionality and analytics.';
-message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// //To put on the page, we must insert it.
+// // message.textContent =
+// //   'We use cookies for improved functionality and analytics.';
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-const header = document.querySelector('.header');
-// header.prepend(message);
-//Prepend makes it the first child of the DOM element
-// header.append(message); // Makes it the last child
-// header.append(message.cloneNode(true));
+// const header = document.querySelector('.header');
+// // header.prepend(message);
+// //Prepend makes it the first child of the DOM element
+// // header.append(message); // Makes it the last child
+// // header.append(message.cloneNode(true));
 
-header.before(message);
-// header.after(message);
+// header.before(message);
+// // header.after(message);
 
-//Delete Elements - deletes once the button is clicked
-document
-  .querySelector('.btn--close-cookie') //we could use the classList as well.
-  .addEventListener('click', function () {
-    message.remove();
-  });
+// //Delete Elements - deletes once the button is clicked
+// document
+//   .querySelector('.btn--close-cookie') //we could use the classList as well.
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
 
-///////// Styles, Attributes, and Classes - use the exact string
-////these are set as inline styles.
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// ///////// Styles, Attributes, and Classes - use the exact string
+// ////these are set as inline styles.
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
 
-console.log(message.style.backgroundColor);
-//We can only retrieve inline styles that we have set ourselves
-//If we want to see the values in the CSS we can use getComputedStyle()
+// console.log(message.style.backgroundColor);
+// //We can only retrieve inline styles that we have set ourselves
+// //If we want to see the values in the CSS we can use getComputedStyle()
 
-console.log(getComputedStyle(message));
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+// console.log(getComputedStyle(message));
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
 
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
 
-console.log(getComputedStyle(message).height);
-// document.documentElement.style.setProperty('--color-primary', 'orangered');
-//You can set any properties this way.
+// console.log(getComputedStyle(message).height);
+// // document.documentElement.style.setProperty('--color-primary', 'orangered');
+// //You can set any properties this way.
 
-////// Attributes //////
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
-// You can read the standard properties that are expected with the type of element, but not ones that you add that are non-standard.
-//but you can access the non-standards by using logo.getAttribute('<attribute>')
+// ////// Attributes //////
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
+// // You can read the standard properties that are expected with the type of element, but not ones that you add that are non-standard.
+// //but you can access the non-standards by using logo.getAttribute('<attribute>')
 
-//Setting Attributes
-// logo.alt = 'Beautiful minimalist logo';
-logo.setAttribute('company', 'Bankist'); //Sets an attribute
+// //Setting Attributes
+// // logo.alt = 'Beautiful minimalist logo';
+// logo.setAttribute('company', 'Bankist'); //Sets an attribute
 
-console.log(logo.src);
-console.log(logo.getAttribute('src'));
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
 
-//links
-const link = document.querySelector('.twitter-link');
-console.log(link.href);
-console.log(link.getAttribute('href'));
+// //links
+// const link = document.querySelector('.twitter-link');
+// console.log(link.href);
+// console.log(link.getAttribute('href'));
+
+// // Data Attributes
+// console.log(logo.dataset.versionNumber);
+
+// // Classes
+// logo.classList.add('test', 'j');
+// logo.classList.remove('test-class');
+// logo.classList.toggle('test-class');
+// console.log(logo.classList.contains('test-class'));
+// console.log(logo.classList.toggle('test-class'));
+
+// console.log(logo.classList);
