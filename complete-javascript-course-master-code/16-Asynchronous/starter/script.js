ed45ba6f7d85e77f82e83713391bfaaadcfb6fe9
wallst-code <senit@senitlutgen.com>
2022-03-02 09:55:34 -0800

    async lesson

--- complete-javascript-course-master-code/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master-code/16-Asynchronous/starter/script.js
@@ -4,3 +4,37 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+// use this URL    https://restcountries.com/v3.1/
+///////////////////////////////////////
+
+// There are many ways to call APIs.
+// Old School API Call Method - HTTP XML Function
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `<article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${data.population}</p>
+          <p class="country__row"><span>🗣️</span>${data.languages}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
+
+        </div>
+      </article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('germany');
+getCountryData('usa');
