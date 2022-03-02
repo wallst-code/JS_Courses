2e0ac8327c9adae0262c78a9ab6612d94ae0d514
wallst-code <senit@senitlutgen.com>
2022-03-02 13:27:30 -0800

    async lessons

--- complete-javascript-course-master-code/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master-code/16-Asynchronous/starter/script.js
@@ -19,15 +19,24 @@ const getCountryData = function (country) {
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
+    const languagesArray = Object.values(data.languages);
+    const currenciesArray = Object.values(data.currencies);
+    console.log(languagesArray[0]);
+    console.log(currenciesArray);
+    console.log(Object.entries(data.currencies));
 
     const html = `<article class="country">
-        <img class="country__img" src="${data.flag}" />
+        <img class="country__img" src="${data.flags.png}" />
         <div class="country__data">
-          <h3 class="country__name">${data.name}</h3>
+          <h3 class="country__name">${data.name.common}</h3>
           <h4 class="country__region">${data.region}</h4>
-          <p class="country__row"><span>👫</span>${data.population}</p>
-          <p class="country__row"><span>🗣️</span>${data.languages}</p>
-          <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} Million</p>
+          <p class="country__row"><span>🗣️</span>${languagesArray[0]}</p>
+          <p class="country__row"><span>💰</span>${currenciesArray[0].symbol} ${
+      currenciesArray[0].name
+    }</p>
 
         </div>
       </article>`;
