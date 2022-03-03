633adebe3aa164732ebb1908c70013f84d557b98
wallst-code <senit@senitlutgen.com>
2022-03-02 16:27:50 -0800

    callback hell

--- complete-javascript-course-master-code/16-Asynchronous/starter/script.js
+++ complete-javascript-course-master-code/16-Asynchronous/starter/script.js
@@ -10,22 +10,11 @@ const countriesContainer = document.querySelector('.countries');
 // There are many ways to call APIs.
 // Old School API Call Method - HTTP XML Function
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    const languagesArray = Object.values(data.languages);
-    const currenciesArray = Object.values(data.currencies);
-    console.log(languagesArray[0]);
-    console.log(currenciesArray);
-    console.log(Object.entries(data.currencies));
-
-    const html = `<article class="country">
+const renderCountry = function (data, className = '') {
+  const languagesArray = Object.values(data.languages);
+  const currenciesArray = Object.values(data.currencies);
+  const html = `
+  <article class="country ${className}">
         <img class="country__img" src="${data.flags.png}" />
         <div class="country__data">
           <h3 class="country__name">${data.name.common}</h3>
@@ -35,15 +24,44 @@ const getCountryData = function (country) {
           ).toFixed(1)} Million</p>
           <p class="country__row"><span>🗣️</span>${languagesArray[0]}</p>
           <p class="country__row"><span>💰</span>${currenciesArray[0].symbol} ${
-      currenciesArray[0].name
-    }</p>
+    currenciesArray[0].name
+  }
+  </p></div>       
+  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
-        </div>
-      </article>`;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+const getCountryDataAndNeighbor = function (country) {
+  // AJAX call Country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  // Event Listener
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render Country 1
+    renderCountry(data);
+
+    // Get Neighbor Country
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // 2nd AJAX Call
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('germany');
-getCountryData('usa');
+getCountryDataAndNeighbor('usa');
